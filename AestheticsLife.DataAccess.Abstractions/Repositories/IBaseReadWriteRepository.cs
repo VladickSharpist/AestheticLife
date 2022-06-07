713fa87679fc548f6178c4258b4a0f9a86b3105e
uladzislau ponpa <uladzislau.ponpa@itechart-group.com>
2022-06-07 08:12:02 +0300

    Set up base rebositories

--- AestheticsLife.DataAccess.Abstractions/Repositories/IBaseReadWriteRepository.cs
+++ AestheticsLife.DataAccess.Abstractions/Repositories/IBaseReadWriteRepository.cs
@@ -1,6 +1,24 @@
+using AestheticLife.DataAccess.Domain.Abstractions.Interfaces;
+
 namespace AestheticsLife.DataAccess.Abstractions;
 
-public interface IBaseReadWriteRepository<TEntity>
+public interface IBaseReadWriteRepository<TEntity> 
+    : IBaseReadonlyRepository<TEntity>
+    where TEntity : class, IEntity
 {
-    
+    void Save(TEntity model);
+
+    Task SaveAsync(TEntity model);
+
+    void SaveChanges();
+
+    Task SaveChangesAsync();
+
+    void Remove(TEntity model);
+        
+    Task RemoveAsync(TEntity model);
+
+    void Remove(long id);
+        
+    Task RemoveAsync(long id);
 }
